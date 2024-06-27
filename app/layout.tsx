f51f0c387b9797fe9b33219a002c41ded1a8a9d6
Andronico Nucum <nicounwntdmain@gmail.com>
2024-06-28 00:40:34 +0800

    merged backend-frontend 6/28/2024

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import './globals.css'
 import SessionProvider from './SessionProvider';
-
+import Nav from "@/app/components/user/homepage"
 
 export default async function RootLayout({
   children,
@@ -8,17 +8,13 @@ export default async function RootLayout({
   children: React.ReactNode
 }) {
   return (
-<<<<<<< HEAD
-    <html lang="en" className="bg-[#aeaeae]">
-      <body className={inter.className}>{children}</body>
-=======
     <html lang="en" className="h-full bg-gray-900">
       <body className="h-full">
+      <Nav />
       <SessionProvider>
         {children}
       </SessionProvider>
       </body>
->>>>>>> 13b274600f3fa8259760278c632bc1b71caf2ee3
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
-<<<<<<< HEAD
 
-import Sidenav from "@/app/sidebar/page"
-=======
 'use client';
+import Sidenav from "@/app/components/user/homepage"
 import { signOut, useSession } from 'next-auth/react';
 import { redirect } from 'next/navigation';
->>>>>>> 13b274600f3fa8259760278c632bc1b71caf2ee3
 
 export default function Home() {
   const session = useSession({
@@ -14,19 +11,14 @@ export default function Home() {
       redirect('/signin');
     },
   });
+
+
   return (
-<<<<<<< HEAD
-    <main>
-    <Sidenav />
-    </main>
-  );
-=======
     <div className="p-8">
       <div className='text-white'>{session?.data?.user?.email }</div>
       <button className='text-white' onClick={() => signOut()}>Logout</button>
     </div>
   )
->>>>>>> 13b274600f3fa8259760278c632bc1b71caf2ee3
 }
 
 Home.requireAuth = true
